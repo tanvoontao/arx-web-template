c73e1a876ac632761f03971e29bb78bc01a6e08d
tanvoontao <tanvoontao0908@gmail.com>
2023-02-21 08:57:29 +0800

    remove aos sample

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,9 @@
 
 import Metatags from '@/components/Metatags/Metatags'
 import UserLayout from '@/components/Layout/UserLayout';
-import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 
 export default function Home() {
-    const [showElement, setShowElement] = useState(false);
-
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setShowElement(true);
-        }, 1); // set delay for 1 ms
-        return () => clearTimeout(timeoutId);
-    }, []);
 
 
     return (
@@ -28,17 +19,7 @@ export default function Home() {
                 <Button variant="contained">test</Button>
 
 
-                {showElement && <main>
-                    <div
-                        data-aos="fade-right"
-                        className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-                        <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
-                        <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-                            The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-                        </p>
-                    </div>
-                </main>}
-
+                
 
             </UserLayout>
         </>
